b7424a45e195d2100949298409fd9a5fdd876b6d
zhuyi55 <43254992+zhuyi55@users.noreply.github.com>
2018-11-18 17:41:27 +0800

    update

--- web_server/myweb/myapp/views.py
+++ web_server/myweb/myapp/views.py
@@ -4,7 +4,7 @@ from django.db import models
 from PIL import Image
 import os
 from io import BytesIO
-import sample2
+#import sample2
 import im2txt.run_inference_api as imModel
 import json
 
@@ -15,38 +15,57 @@ class Images(models.Model):
 def index(request):
     return render(request, "index.html")
     
+global caption_generator
+caption_generator = None
+
 #@csrf_exempt
 def uploadImg(request):
+    global caption_generator
+    baseDir = os.path.dirname(os.path.abspath(__name__));
+    jpgdir = os.path.join(baseDir, 'static');
+    chkpoint_path = os.path.join(baseDir, 'im_model')
+    vocab = os.path.join(chkpoint_path, 'word_counts.txt')
+        
+    if (caption_generator == None):
+        caption_generator = imModel.run_inference_api(checkpoint_path=chkpoint_path, vocab_file=vocab)
+        
     if request.method == 'POST':
-        img = request.FILES.get('img')
+        img_name_list = []
+        result1_list = []
+        result2_list = []
         
-        baseDir = os.path.dirname(os.path.abspath(__name__));
-        jpgdir = os.path.join(baseDir, 'static');
-        
-        filename = os.path.join(jpgdir,img.name);
-        fobj = open(filename,'wb');
-        for chrunk in img.chunks():
-            fobj.write(chrunk);
-        fobj.close();
-        
-        chkpoint_path = os.path.join(baseDir, 'im_model')
-        vocab = os.path.join(chkpoint_path, 'word_counts.txt')
-        
-        strlist = imModel.function(checkpoint_path=chkpoint_path, vocab_file=vocab, input_files=filename)
-        #strlist = sample2.prediction()
-        #im_pic = Image.open(BytesIO(img))
-        #im_pic.save(filename)
+        imglist = request.FILES.getlist('img')
+        img_cnt = len(imglist)
+        for idx in range(img_cnt):
+            img = imglist[idx]
+
+            filename = os.path.join(jpgdir,img.name);
+            with open(filename,'wb') as fobj:
+                for chrunk in img.chunks():
+                    fobj.write(chrunk);
+
+            print (filename)
+            strlist = caption_generator.generate(filename)
+            #strlist = sample2.prediction()
+            img_name_list.append(img.name)
+            result1_list.append(strlist[0])
+            result2_list.append(strlist[1])
+            
+            #im_pic = Image.open(BytesIO(img))
+            #im_pic.save(filename)
+            
         content = {
-            'result2':strlist[0],
-            'result2':strlist[1],
-            'img': img.name
+            'result1':json.dumps(result1_list),
+            'result2':json.dumps(result2_list),
+            'imgView': img_name_list[0]
+            #'imgView': json.dumps(img_name_list)
         }
-        return render(request, 'uploadimg.html', {
-            'result1': json.dumps(strlist[0]),
-            'result2': json.dumps(strlist[1]),
-            'imgView': img.name
-            })
-        #return render (request, 'showImg.html', content)
+        #return render(request, 'uploadimg.html', {
+        #    'result1': json.dumps(strlist[0]),
+        #    'result2': json.dumps(strlist[1]),
+        #    'imgView': img.name
+        #    })
+        return render (request, 'uploadimg.html', content)
 
     return render(request, 'uploadimg.html')
     
